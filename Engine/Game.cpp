cfe52845d3ad4e1cd7ce394482bb704b62257a06
anshu2950 <anshu2950@gmail.com>
2023-03-25 20:24:33 +0530

    engine

---
+++ Engine/Game.cpp
@@ -0,0 +1,26 @@
+
+#include "MainWindow.h"
+#include "Game.h"
+
+Game::Game( MainWindow& wnd )
+	:
+	wnd( wnd ),
+	gfx( wnd )
+{
+}
+
+void Game::Go()
+{
+	gfx.BeginFrame();	
+	UpdateModel();
+	ComposeFrame();
+	gfx.EndFrame();
+}
+
+void Game::UpdateModel()
+{
+}
+
+void Game::ComposeFrame()
+{
+}
